5705f9ddd3352de24527555b5af52ba5d16658fd
Rakesh1186 <rakesh.tashildar@gmail.com>
2024-01-15 22:40:25 +0530

    Project created

---
+++ src/main/java/com/optiC/optiC/OptiCApplication.java
@@ -0,0 +1,13 @@
+package com.optiC.optiC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optiC/optiC/OptiCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optiC.optiC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
